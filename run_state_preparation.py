a10cb5b0fe5d1a83ed97a667a90a6344ad8cbfa8
Colin_Campbell_CQ <colin.campbell@coldquanta.com>
2024-10-08 11:56:28 -0500

    collected data for n=2-4 for m=n for all available methods, and n=5-9 for dcsp

--- run_state_preparation.py
+++ run_state_preparation.py
@@ -119,11 +119,11 @@ def run_prepare_state(method):
     path_finder = PathFinderSHP()
     # path_finder = PathFinderMST()
     # num_qubits_all = np.array([5])
-    num_qubits_all = np.array(list(range(5, 12)))
+    num_qubits_all = np.array(list(range(2, 5)))
     num_amplitudes_all = num_qubits_all # possible values are [num_qubits_all, num_qubits_all**2, 2**(num_qubits_all-1)]
     # out_col_name = "qiskit"
     out_col_name = method
-    num_workers = 4
+    num_workers = 6
     reduce_controls = True
     check_fidelity = True
     remove_leading_cx = True
@@ -189,7 +189,7 @@ if __name__ == "__main__":
     # merge_state_files()
     all_methods = list(METHODS.keys())
     # run_prepare_state('topdown')
-    for method in all_methods:
+    for method in all_methods[4:]:  # Just trying dcsp on its own since topdown took so long
         print(f"Starting method: {method}")
         try:
             run_prepare_state(method)
